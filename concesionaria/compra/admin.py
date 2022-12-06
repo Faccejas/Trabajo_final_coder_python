ebf5409babea739510abf21a66cb27835b68087a
Facundo Cejas <facunahuelcejas@gmail.com>
2022-12-05 22:49:15 -0300

    Creación del usuario en el panel de admin

--- concesionaria/compra/admin.py
+++ concesionaria/compra/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from compra.models import *
 
 # Register your models here.
+
+admin.site.register(Vendedor)
+
+admin.site.register(Rodado)
+
--- concesionaria/venta/admin.py
+++ concesionaria/venta/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from venta.models import *
 
 # Register your models here.
+
+admin.site.register(Comprador)
+
+admin.site.register(Rodado)
+
+admin.site.register(Aseguradora)
+
+
