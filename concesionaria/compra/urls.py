3732064a124b81d94f9e9af3e0be0dde49ad3d9f
Facundo Cejas <facunahuelcejas@gmail.com>
2022-12-08 12:33:03 -0300

    Formularios agregados & clases

--- concesionaria/compra/urls.py
+++ concesionaria/compra/urls.py
@@ -4,5 +4,11 @@ from compra.views import *
 urlpatterns = [
     path("Inicio/", Inicio, name= "compra-inicio"),
     path("Automovil/", Automovil, name= "compra-automovil"),
-    path("Clientes/", Clientes, name= "compra-clientes")
+    path("Clientes/", Clientes, name= "compra-clientes"),
+    
+    path("automovil/", AutomovilList.as_view(), name= "compra-automovil"),
+    path("automovil/detalle/<pk>/", AutomovilDetail.as_view(), name= "compra-automovil-detail"),
+    path("automovil/crear/", AutomovilCreate.as_view(), name= "compra-automovil-create"),
+    path("automovil/actualizar/<pk>/", AutomovilUpdate.as_view(), name= "compra-automovil-update"),
+    path("automovil/borrar/<pk>/", AutomovilDelete.as_view(), name= "compra-automovil-delete")
 ]
--- concesionaria/compra/views.py
+++ concesionaria/compra/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from compra.models import *
+from compra.forms import *
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 # Create your views here.
 
@@ -12,3 +14,25 @@ def Automovil(request):
 
 def Clientes(request):
     return render(request, "compra/Clientes.html")
+
+class AutomovilList(ListView):
+    model = Rodado
+    template_name = "compra/list_automovil.html"
+
+class AutomovilDetail(DetailView):
+    model = Rodado
+    template_name = "compra/detail_automovil.html"
+
+class AutomovilCreate(CreateView):
+    model = Rodado
+    success_url = "/compra/automovil/"
+    fields = ["marca", "modelo" , "color", "km", "año", "precio", "patente", "vtv_hecha"]
+
+class AutomovilUpdate(UpdateView):
+    model = Rodado
+    success_url = "/compra/automovil/"
+    fields = ["marca", "modelo" , "color", "km", "año", "precio", "patente", "vtv_hecha"]
+
+class AutomovilDelete(DeleteView):
+    model = Rodado
+    success_url = "/compra/automovil/"
