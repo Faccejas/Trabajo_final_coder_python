7c5a992e7249206ad3107c816f33679dc4cc9a19
Facundo Cejas <facunahuelcejas@gmail.com>
2022-12-10 15:38:52 -0300

    add "comprador"

--- concesionaria/venta/urls.py
+++ concesionaria/venta/urls.py
@@ -4,13 +4,20 @@ from venta.views import *
 urlpatterns = [
     #path("Inicio/", Inicio, name= "venta-inicio"),
     #path("Automovil/", Automovil, name= "venta-automovil"),
-    path("Clientes/", Clientes, name= "venta-clientes"),
+    #path("Clientes/", Clientes, name= "venta-clientes"),
     path("Seguros/", Seguro, name= "venta-seguros"),
 
     path("automovil/", AutomovilList.as_view(), name= "venta-automovil"),
     path("automovil/detalle/<pk>/", AutomovilDetail.as_view(), name= "venta-automovil-detail"),
     path("automovil/crear/", AutomovilCreate.as_view(), name= "venta-automovil-create"),
     path("automovil/actualizar/<pk>/", AutomovilUpdate.as_view(), name= "venta-automovil-update"),
-    path("automovil/borrar/<pk>/", AutomovilDelete.as_view(), name= "venta-automovil-delete")
+    path("automovil/borrar/<pk>/", AutomovilDelete.as_view(), name= "venta-automovil-delete"),
+
+    path("comprador/", CompradorList.as_view(), name= "venta-comprador"),
+    path("comprador/detalle/<pk>/", CompradorDetail.as_view(), name= "venta-comprador-detail"),
+    path("comprador/crear/", CompradorCreate.as_view(), name= "venta-comprador-create"),
+    path("comprador/actualizar/<pk>/", CompradorUpdate.as_view(), name= "venta-comprador-update"),
+    path("comprador/borrar/<pk>/", CompradorDelete.as_view(), name= "venta-comprador-delete")
+
 
 ]
--- concesionaria/venta/views.py
+++ concesionaria/venta/views.py
@@ -11,8 +11,8 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 #def Automovil(request):
     #return render(request, "venta/Automovil.html")
 
-def Clientes(request):
-    return render(request, "venta/Clientes.html")
+#def Clientes(request):
+    #return render(request, "venta/Clientes.html")
 
 def Seguro(request):
     return render(request, "venta/Seguro.html")
@@ -40,3 +40,28 @@ class AutomovilUpdate(UpdateView):
 class AutomovilDelete(DeleteView):
     model = Rodado
     success_url = "/venta/automovil/"
+
+
+
+class CompradorList(ListView):
+    model = Comprador
+    template_name = "venta/list_comprador.html"
+
+class CompradorDetail(DetailView):
+    model = Comprador
+    template_name = "venta/detail_comprador.html"
+
+class CompradorCreate(CreateView):
+    model = Comprador
+    success_url = "/venta/comprador/"
+    fields = ["nombre", "apellido" , "dni", "domicilio", "localidad", "celular", "email"]
+    template_name = "venta/comprador_form.html"
+    
+class CompradorUpdate(UpdateView):
+    model = Comprador
+    success_url = "/compra/comprador/"
+    fields = ["nombre", "apellido" , "dni", "domicilio", "localidad", "celular", "email"]
+
+class CompradorDelete(DeleteView):
+    model = Comprador
+    success_url = "/venta/comprador/"
